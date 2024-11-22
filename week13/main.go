875a4501ac033aa5615045b5c9146cc4e1692613
hyeon <smyoun115@gmail.com>
2024-11-22 15:13:34 +0900

    v4.4 Slice

--- week13/main.go
+++ week13/main.go
@@ -1,23 +1,35 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// )
+
+// func main() {
+// 	// fmt.Println(os.Args[1:], len(os.Args))
+// 	slices := os.Args[1:]
+// 	fmt.Println(slices[1])
+
+//		for _, slice := range slices {
+//			fmt.Println(slice)
+//		}
+//		slices = append(slices, "forever", ":")
+//		fmt.Println(slices, len(slices))
+//	}
 package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	var emptySlice []bool
-	emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slic3 zero value (nil), 0go ru
-
-	if len(emptySlice) == 5 {
-		emptySlice = append(emptySlice, true)
+	//fmt.Println(os.Args[1:], len(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices[1])
+	for _, slice := range slices {
+		fmt.Println(slice)
 	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice))
-
-	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
-	gpa_slice := gpa[1:4] // 4.1, 4.5, 3.9
-	// gpa_slice[2] = 2.71
-	gpa[2] = 2.71
-	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpa)
+	slices = append(slices, "forever", "!")
+	fmt.Println(slices, len(slices))
 }
