186271f8d2a2a1cca829f31def0a04e2520c2545
hyeon <smyoun115@gmail.com>
2024-11-22 14:58:04 +0900

    v4.3 Slice

--- week13/main.go
+++ week13/main.go
@@ -6,8 +6,13 @@ import (
 
 func main() {
 	var emptySlice []bool
-	// emptySlice = make([]int, 5)
-	fmt.Printf("%#v\n", emptySlice)
+	emptySlice = make([]bool, 5)
+	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slic3 zero value (nil), 0go ru
+
+	if len(emptySlice) == 5 {
+		emptySlice = append(emptySlice, true)
+	}
+	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice))
 
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
 	gpa_slice := gpa[1:4] // 4.1, 4.5, 3.9
