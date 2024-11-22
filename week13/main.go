dc79698c1312a0af7145d2ff9db6a82651203182
hyeon <smyoun115@gmail.com>
2024-11-22 15:20:13 +0900

    v4.5 Slice

--- week13/main.go
+++ week13/main.go
@@ -21,7 +21,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
 )
+
+func test(strs ...string) {
+	fmt.Println(strs, reflect.TypeOf(strs))
+}
 
 func main() {
 	//fmt.Println(os.Args[1:], len(os.Args))
@@ -32,4 +37,8 @@ func main() {
 	}
 	slices = append(slices, "forever", "!")
 	fmt.Println(slices, len(slices))
+	test("abc")
+	test("abc", "123")
+	test()
+	test("abc", "123", "inha")
 }
